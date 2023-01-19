6df38b2478b21c46d46e4d0ae1b407461847a36e
BroJJun <dofun8@gmail.com>
2023-01-19 17:54:06 +0900

    알고리즘 풀던거 저장 // 문제 - 입력을 무한히 받음

--- 02-JAVA/algorithm/BOJ/src/Main.java
+++ 02-JAVA/algorithm/BOJ/src/Main.java
@@ -1,11 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
-import java.util.concurrent.SynchronousQueue;
+
 
 /*
 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,37 +14,23 @@ int bag = Integer.parseInt(yxbag[2]);
  */
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	
-		
-		int n = Integer.parseInt(br.readLine());
-		ArrayList<String> book = new ArrayList<String>();
-		
-		for(int i = 0; i < n;i++) {
-			String word = br.readLine();
-			book.add(word);
-		}
-		Collections.sort(book);
-		
-		String[][] arr = new String[51][20000];
-		int[] size = new int[51];
-		while(!book.isEmpty()) {
-			String word = book.get(0);
-			arr[word.length()][size[word.length()]++] = word;
-			book.remove(0);
-		}
-		
-		for(int i = 0; i < 51; i++) {
-			String words = "";
-			for(int j = 0; j < size[i]; j++) {
-				if(words.equals(arr[i][j]))
-					continue;
-				words = arr[i][j];
-				System.out.println(arr[i][j]);
 
-			}
-			
+		
+		long a1 = Integer.parseInt(br.readLine());
+		long a2 = Integer.parseInt(br.readLine());
+		long a3 = Integer.parseInt(br.readLine());
+		
+		
+		long a4 = a1 * a2 * a3;
+		int[] numberMap = new int[10];
+		while(a4 > 0) {
+			int a = (int) a4 % 10;
+			numberMap[a]++;
+			a /= 10;
 		}
+		
+		for(int i = 1; i < 10; i++)System.out.println(numberMap[i]);
 	}
 }
