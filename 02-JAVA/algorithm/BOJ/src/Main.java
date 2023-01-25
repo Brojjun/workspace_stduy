221dd2c3be928b04e7fe1022c55b43095985d733
BroJJun <dofun8@gmail.com>
2023-01-25 17:34:36 +0900

    1.25 Exception 추가 변경사항

--- 02-JAVA/algorithm/BOJ/src/Main.java
+++ 02-JAVA/algorithm/BOJ/src/Main.java
@@ -1,40 +1,40 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.Arrays;
-
-
-/*
-BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-String[] yxbag = br.readLine().split(" ");
-
- */
-
-public class Main {
-		
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int[] numberMap = new int[1000002];
-		
-		//소수 표기
-		for(int i = 2; i <= 1000000; i++) {
-			if(numberMap[i] >= 1)continue;
-			numberMap[i] = 2;
-			for(int j = i;j <= 1000000; j += i) {
-				if(numberMap[j] == 0) numberMap[j] = 1;
+	
+	import java.io.BufferedReader;
+	import java.io.IOException;
+	import java.io.InputStreamReader;
+	import java.math.BigInteger;
+	import java.util.Arrays;
+	
+	
+	/*
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	String[] yxbag = br.readLine().split(" ");
+	
+	 */
+	
+	public class Main {
+			
+		public static void main(String[] args) throws Exception {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+			int[] numberMap = new int[1000002];
+			
+			//소수 표기
+			for(int i = 2; i <= 1000000; i++) {
+				if(numberMap[i] >= 1)continue;
+				numberMap[i] = 2;
+				for(int j = i;j <= 1000000; j += i) {
+					if(numberMap[j] == 0) numberMap[j] = 1;
+				}
+			}
+			
+			String[] imm = br.readLine().split(" ");
+			int sn = Integer.parseInt(imm[0]);
+			int fn = Integer.parseInt(imm[1]);
+			
+			for(int i = sn; i <= fn; i++) {
+				if(numberMap[i] == 2) System.out.println(i);
+				
 			}
 		}
-		
-		String[] imm = br.readLine().split(" ");
-		int sn = Integer.parseInt(imm[0]);
-		int fn = Integer.parseInt(imm[1]);
-		
-		for(int i = sn; i <= fn; i++) {
-			if(numberMap[i] == 2) System.out.println(i);
-			
-		}
 	}
-}
