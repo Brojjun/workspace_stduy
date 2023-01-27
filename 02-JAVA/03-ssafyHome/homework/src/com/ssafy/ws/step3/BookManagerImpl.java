b120134c1f8602ea66b233759160c203bd370099
BroJJun <dofun8@gmail.com>
2023-01-27 15:50:29 +0900

    과제 중 ( 10-3 )

--- 02-JAVA/03-ssafyHome/homework/src/com/ssafy/ws/step3/BookManagerImpl.java
+++ 02-JAVA/03-ssafyHome/homework/src/com/ssafy/ws/step3/BookManagerImpl.java
@@ -1,9 +1,12 @@
 package com.ssafy.ws.step3;
 
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -13,11 +16,11 @@ import java.util.List;
 /**
  * 도서리스트를 컬렉션으로 유지하며 관리하는 클래스
  */
-public class BookManagerImpl implements IBookManager{
+public class BookManagerImpl implements IBookManager {
 
-	List<Book>books = new ArrayList<>();
+	List<Book> books = new ArrayList<>();
 	private IBookManager ib = new BookManagerImpl();
-	
+
 	private BookManagerImpl() {
 		super();
 	}
@@ -25,7 +28,7 @@ public class BookManagerImpl implements IBookManager{
 	public IBookManager getInstance() {
 		return this.ib;
 	}
-	
+
 	@Override
 	public void add(Book book) {
 		// TODO Auto-generated method stub
@@ -35,10 +38,9 @@ public class BookManagerImpl implements IBookManager{
 	@Override
 	public void remove(String isbn) {
 		// TODO Auto-generated method stub
-		for(int i = this.books.size(); i >= 0; i--) 
-			if(this.books.get(i).getIsbn().equals(isbn)) 
+		for (int i = this.books.size(); i >= 0; i--)
+			if (this.books.get(i).getIsbn().equals(isbn))
 				this.books.remove(i);
-		
 	}
 
 	@Override
@@ -51,8 +53,8 @@ public class BookManagerImpl implements IBookManager{
 	@Override
 	public Book searchByIsbn(String isbn) {
 		// TODO Auto-generated method stub
-		for(int i = 0; i < books.size(); i++) {
-			if(books.get(i).getIsbn().equals(isbn))
+		for (int i = 0; i < books.size(); i++) {
+			if (books.get(i).getIsbn().equals(isbn))
 				return books.get(i);
 		}
 		return null;
@@ -61,57 +63,91 @@ public class BookManagerImpl implements IBookManager{
 	@Override
 	public Book[] searchByTitle(String title) {
 		// TODO Auto-generated method stub
-		for(int i = 0; i < books.size(); i++) {
-			if(books.get(i).getTitle().equals(title))
-				return books.get(i);
+		Book[] immBooks = new Book[books.size()];
+		int cnt = 0;
+		for (int i = 0; i < books.size(); i++) {
+			if (books.get(i).getTitle().equals(title))
+				immBooks[cnt++] = books.get(i);
 		}
-		return null;
+		return immBooks;
 	}
 
 	@Override
 	public Magazine[] getMagazines() {
 		// TODO Auto-generated method stub
-		return null;
+		Magazine[] magazines = new Magazine[books.size()];
+		int cnt = 0;
+		for (int i = 0; i < books.size(); i++) {
+			if (books.get(i) instanceof Magazine)
+				magazines[cnt++] = (Magazine) books.get(i);
+		}
+		return magazines;
 	}
 
 	@Override
 	public Book[] getBooks() {
 		// TODO Auto-generated method stub
-		return null;
+		Book[] immBooks = new Book[books.size()];
+		int cnt = 0;
+		for (int i = 0; i < books.size(); i++) {
+			if (!(books.get(i) instanceof Magazine))
+				immBooks[cnt++] = books.get(i);
+		}
+		return immBooks;
 	}
 
 	@Override
 	public int getTotalPrice() {
 		// TODO Auto-generated method stub
-		return 0;
+		int price = 0;
+		for (int i = 0; i < books.size(); i++) {
+			price += books.get(i).getPrice();
+		}
+		return price;
 	}
 
 	@Override
 	public double getPriceAvg() {
 		// TODO Auto-generated method stub
-		return 0;
+		int price = 0;
+		for (int i = 0; i < books.size(); i++) {
+			price += books.get(i).getPrice();
+		}
+		return price / books.size();
 	}
 
 	@Override
 	public void sell(String isbn, int quantity) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void buy(String isbn, int quantity) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void loadData() {
-		
+		try {
+			FileInputStream fis = new FileInputStream("book.dat");
+			ObjectInputStream oir = new ObjectInputStream(fis);
+			while (true) {
+				books.add((Book) oir.readObject());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.out.println("더이상 도서 없음");
+		}catch (EOFException e) {
+			System.out.println("더이상 도서 없음");
+		}
 	}
-	
+
 	@Override
 	public void saveData() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	//코드를 작성해주세요. 
+	// 코드를 작성해주세요.
 }
