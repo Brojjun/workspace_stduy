bd3100b3f8f4c682dd0565d61309352f60fdee57
Brojjun <dofun8@gmail.com>
2023-01-31 23:07:26 +0900

    no message

--- 02-JAVA/algorithm/BOJ/src/Main.java
+++ 02-JAVA/algorithm/BOJ/src/Main.java
@@ -9,92 +9,55 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-class Dat{
-	int at;
-	int data;
-	
-	public Dat(int at, int data) {
-		this.at = at;
-		this.data = data;
-	}
-}
 
 public class Main {
+
+	
+	static StringBuffer sb = new StringBuffer();
+
+	
+	static int[] map;
+	static int bn;
+	static long bc;
+	static long end;
+
+
+	public static boolean check(long cut) {
+		long tree = 0;
+		for(int i = 0; i < bn; i++)
+			if(map[i] - cut > 0)
+				tree += (map[i] - cut);
+		
+		return tree >= bc;
+	}
+	
+	
+	public static void binary_search() {
+		long begin = 0;
+		long end = 1000000001;
+		
+		while(begin + 1 < end) {
+			long mid = (begin + end) / 2;
+		
+			if(check(mid)) {
+				begin = mid;
+				//System.out.println(begin +" : " + end);
+			}
+			else end = mid;
+			
+		}
+		System.out.println(begin);
+	}
 	
 
-	static StringBuffer sb = new StringBuffer();
-	
-	static int bn;
-	static int[] map;
-	static int[] cnt;
-	
-	public static void binarySearch(int target) {
-		int begin = 0;
-		int end = bn-1;
-		while(begin <= end) {
-			int mid = (begin + end) / 2;
-			if(map[mid] == target) {
-				findSameNum(mid, target);
-				return;
-			}
-			else if(map[mid] > target) {
-				end = mid - 1;
-			}
-			else
-				begin = mid + 1;
-		}
-	sb.append("0 ");
-	}
-	
-	//bfs를 활용한 같은 숫자 탐색
-	public static void findSameNum(int at, int target) {
-		//재탐색 불허를 위한 방문 표시 -> 없으면 탐색했던 장소를 무한하게 재 탐색
-		boolean[] visited = new boolean[bn];
-		
-		//bfs
-		Queue<Dat> que = new LinkedList<>();
-		int cnt = 1;
-		que.add(new Dat(at ,target));
-		visited[at] = true;
-		while(!que.isEmpty()) {
-			Dat now = que.poll();
-			if(now.at + 1 < bn)
-			if(visited[now.at + 1] == false && map[now.at + 1] == target && now.at < bn ) {
-				visited[now.at + 1] = true;
-				cnt++;
-				que.add(new Dat(at+1, target));
-			}
-			if(now.at -1 >= 0)
-			if(visited[now.at - 1] == false && map[now.at - 1] == target && now.at >= 0 ) {
-				visited[now.at - 1] = true;
-				cnt++;
-				que.add(new Dat(at-1, target));
-			}
-		}
-		sb.append(cnt + " ");		
-	}
-	
-	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated constructor stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		bn = Integer.parseInt(br.readLine());
+		Scanner sc = new Scanner(System.in);
+		bn = sc.nextInt();
+		bc = sc.nextInt();
 		map = new int[bn];
-		String[] imm = br.readLine().split(" "); 
-		for(int i = 0; i < bn; i++) {
-			map[i] = Integer.parseInt(imm[i]);
-		}
-		
+		for(int i = 0; i < bn; i++) {map[i] = sc.nextInt(); end = Math.max(end, map[i]);}
 		Arrays.sort(map);
-	
-		int bm = Integer.parseInt(br.readLine());
-		
-		String[] immm = br.readLine().split(" "); 
-	
-		for(int i = 0; i < bm; i++) {
-			binarySearch(Integer.parseInt(immm[i]));
-		}
-		System.out.println(sb);
+		binary_search();
 		
 	}
 }
