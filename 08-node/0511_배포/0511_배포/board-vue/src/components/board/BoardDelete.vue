b0946c6d79f8b1dd94a22bb433b03185745eec2d
brojjun <dofun8@gmail.com>
2023-05-15 10:18:33 +0900

    add_vue

---
+++ 08-node/0511_배포/0511_배포/board-vue/src/components/board/BoardDelete.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="regist">
+    <h1>SSAFY 글 삭제</h1>
+    <div>삭제중....</div>
+  </div>
+</template>
+
+<script>
+import http from "@/util/http-common"
+export default {
+  name: "BoardDelete",
+  data() {
+    return {
+      articleno: Number,
+    };
+  },
+  created() {
+    // 비동기
+    // TODO : 글번호에 해당하는 글을 삭제.
+    this.articleno = this.$route.params.articleno;
+    http.delete(`/board/${this.articleno}`)
+    .then(({data})=>{
+      let msg = "글 삭제 시 문제 발생~";
+        if(data === "success")
+          msg = "글 삭제 성공"
+        
+        alert(msg);
+
+        this.moveList();
+    })
+
+    console.log(this.articleno + "번글 삭제!!!!!");
+  },
+  methods:{
+  moveList() {
+      console.log("글목록 보러가자!!!");
+      this.$router.push({ name: "boardlist" });
+    },
+  } 
+};
+</script>
+
+<style></style>
---
+++ 08-node/0512d_배포/boot/bootstrap-vue-test/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <nav>
+      <the-header-nav-bar></the-header-nav-bar>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+
+<script>
+import TheHeaderNavBar from './components/TheHeaderNavBar.vue';
+
+export default {
+    name: 'App',
+    components:{
+      TheHeaderNavBar,
+    },
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
